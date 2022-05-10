0f1aa0d1ae7aa180dce942de81d2a08c6a47f398
IdrisGuloyev <idrisguloyev@gmail.com>
2022-05-10 18:29:41 +0600

    Added logic in game

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 'use strict';
+
+const secretNubmer = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.question').textContent = secretNubmer;
+
+let score = 20;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guessingNumber = Number(document.querySelector('.number-input').value);
+  console.log(guessingNumber, typeof guessingNumber);
+
+  if (!guessingNumber) {
+    document.querySelector('.guess-message').textContent = 'Введите число!';
+  } else if (guessingNumber === secretNubmer) {
+    document.querySelector('.guess-message').textContent = 'Правильно!';
+  } else if (guessingNumber > secretNubmer) {
+    if (score > 1) {
+      document.querySelector('.guess-message').textContent = 'Слишком много!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.guess-message').textContent = 'Игра окончена!';
+    }
+  } else if (guessingNumber < secretNubmer) {
+    if (score > 1) {
+      document.querySelector('.guess-message').textContent = 'Слишком мало!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.guess-message').textContent = 'Игра окончена!';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
